5ccdeeb82e716670c14a52d2e51980a19f286d49
John Nielsen <johnhayesnielsen@gmail.com>
2020-12-22 10:28:57 -0700

    added scan screen

--- App.js
+++ App.js
@@ -34,81 +34,8 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <NavigationContainer>
-        {/* Create navigator stack which will hold all "routes".
+      {/* Create navigator stack which will hold all "routes".
       initialRouteName determines default route. */}
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerStyle: { backgroundColor: "#212121" },
-            headerTintColor: "#FFCB6B",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
-        >
-          {/* Add a Stack.Screen component for each screen you want to add to navigation.
-        It requires a name Prop which will be used to navigate to that screen from anywhere,
-        as well as a component prop which tells the navigator which prop to render for that route. */}
-          <Stack.Screen
-            name="FetchTest"
-            component={FetchTest}
-            initialParams={{ name: "Dummy" }}
-            options={{
-              title: "GraphQL Testing",
-            }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              title: "Home",
-            }}
-          />
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{
-              title: "Login",
-            }}
-          />
-          <Stack.Screen
-            name="Create"
-            component={Create}
-            options={{
-              title: "Create",
-            }}
-          />
-          <Stack.Screen
-            name="Profile"
-            component={ProfileScreen}
-            options={({ route }) => ({
-              title: route.params.name,
-            })}
-          />
-          <Stack.Screen
-            name="Signup"
-            component={SignupScreen}
-            options={({ route }) => ({
-              title: route.params.name,
-            })}
-          />
-          <Stack.Screen
-            name="Contact"
-            component={Contact}
-            options={({ route }) => ({
-              title: route.params.name,
-            })}
-          />
-          <Stack.Screen
-            name="Collection"
-            component={Collection}
-            options={({ route }) => ({
-              title: "Collection",
-            })}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name='Home' component={Home} />
