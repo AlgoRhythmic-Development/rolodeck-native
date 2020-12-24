4cb59161667566e430c4beb77e1b39bc19c7464d
probison <parker.robison@hotmail.com>
2020-12-23 23:02:57 -0700

    added formik, started woking on signup form, used Contact.js to test.

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import ContactForm from '../components/ContactForm';
 import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Formik } from 'formik';
 
 const Contact = () => {
 
@@ -11,6 +12,38 @@ const Contact = () => {
                 </Text>
             <View>
                 {/* <ContactForm></ContactForm> */}
+                {/* <StatusBar style='auto' /> */}
+                <Text> Login Form </Text>
+                <View>
+                    <Formik 
+                    initialValues={{ username: '', email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log(values)
+                    }}
+                    >
+                    {(props)=> (
+                        <View>
+                        <TextInput
+                            placeholder='Username'
+                            onChangeText={props.handleChange('username')}
+                            value={props.values.username}
+                        />
+                        <TextInput
+                            placeholder='Email'
+                            onChangeText={props.handleChange('email')}
+                            value={props.values.email}
+                        />
+                        <TextInput
+                            placeholder='Password'
+                            onChangeText={props.handleChange('password')}
+                            value={props.values.password}
+                        />
+
+                        <Button title='submit' onPress={props.handleSumbit} />
+                        </View>
+                    )}      
+                    </Formik>
+                </View>
             </View>
 
         </View >
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { StatusBar } from "expo-status-bar";
 import { Link } from '@react-navigation/native';
+import { Formik } from 'formik';
 
 const SignupScreen = () => {
   // const [formState, setFormState] = useState({
@@ -15,6 +16,37 @@ const SignupScreen = () => {
       <StatusBar style='auto' />
       <Text> Login Form </Text>
       <View>
+        <Formik 
+          initialValues={{ username: '', email: '', password: '' }}
+          onSubmit={(values) => {
+            console.log(values)
+          }}
+        >
+          {(props)=> (
+            <View>
+              <TextInput
+                placeholder='Username'
+                onChangeText={props.handleChange('username')}
+                value={props.values.username}
+              />
+              <TextInput
+                placeholder='Email'
+                onChangeText={props.handleChange('email')}
+                value={props.values.email}
+              />
+              <TextInput
+                placeholder='Password'
+                onChangeText={props.handleChange('password')}
+                value={props.values.password}
+              />
+
+              <Button title='submit' onPress={props.handleSumbit}></Button>
+            </View>
+          )}      
+        </Formik>
+      </View>
+      
+      {/* <View>
         <TextInput placeeholder="Enter Username" />
         <TextInput placeholder="Enter Email" />
         <TextInput
@@ -22,15 +54,15 @@ const SignupScreen = () => {
           placeholder="Enter Password"
         />
         <Button title="Submit"></Button>
-      </View>
-      <View>
+      </View> */}
+      {/* <View> */}
         {/* This Link will need to hook up to the login page */}
-        <Text>Already have an account? Head to our <Link style={styles.linkText} to="/Login">Login page</Link></Text>
+        {/* <Text>Already have an account? Head to our <Link style={styles.linkText} to="/Login">Login page</Link></Text>
       </View>
-      <View>
+      <View> */}
         {/* This Link will need to hook up to the Home page */}
-        <Text><Link style={styles.linkText} to="/Home">Home</Link></Text>
-      </View>
+        {/* <Text><Link style={styles.linkText} to="/Home">Home</Link></Text>
+      </View> */}
 
     </View>
 
