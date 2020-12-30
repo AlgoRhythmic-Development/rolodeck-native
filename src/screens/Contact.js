38c698f4546d58e0e291a38423f6866c49068c41
probison <parker.robison@hotmail.com>
2020-12-29 22:07:56 -0700

    signup form now passes keyed in data

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -3,51 +3,59 @@ import React from 'react';
 import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Formik } from 'formik';
 
-const Contact = () => {
+// all of this was commented out because I needed this page to test my code since login wasn't working. 
+// const Contact = () => {
 
-    return (
+//     return (
+//         <View>
+//             <Text>Contact Us</Text>
+//             <Text>Fill out the form below to contact us. We'd love to hear your questions, feedback, and ideas. After submiting this form, we will contact you as soon as possible.
+//                 </Text>
+//             <View>
+//                 {/* <ContactForm></ContactForm> */}
+//             </View>
+
+//         </View >
+//     )
+// };
+// export default Contact;
+
+// this form will need styles from a global styles sheet. The function name needs to be changed as well.
+export default function Contact() {
+    return(
         <View>
-            <Text>Contact Us</Text>
-            <Text>Fill out the form below to contact us. We'd love to hear your questions, feedback, and ideas. After submiting this form, we will contact you as soon as possible.
-                </Text>
-            <View>
-                {/* <ContactForm></ContactForm> */}
-                {/* <StatusBar style='auto' /> */}
-                <Text> Login Form </Text>
-                <View>
-                    <Formik 
-                    initialValues={{ username: '', email: '', password: '' }}
-                    onSubmit={(values) => {
-                        console.log(values)
-                    }}
-                    >
-                    {(props)=> (
-                        <View>
+            <Text>Sign Up Form</Text>
+            <Formik
+                initialValues={{ username: '', email: '', password: '' }}
+                onSubmit={(values, actions) => {
+                    actions.resetForm();
+                    console.log(values);
+                }}
+            >
+                {(props) => (
+                    <View>
                         <TextInput
-                            placeholder='Username'
+                            placeholder='Enter a username'
                             onChangeText={props.handleChange('username')}
-                            value={props.values.username}
-                        />
+                            value={props.values.username} 
+                        />  
+
                         <TextInput
-                            placeholder='Email'
+                            placeholder='Enter your email'
                             onChangeText={props.handleChange('email')}
-                            value={props.values.email}
-                        />
+                            value={props.values.email} 
+                        />  
+
                         <TextInput
-                            placeholder='Password'
+                            placeholder='Create a password'
                             onChangeText={props.handleChange('password')}
-                            value={props.values.password}
-                        />
+                            value={props.values.password} 
+                        />   
 
-                        <Button title='submit' onPress={props.handleSumbit} />
-                        </View>
-                    )}      
-                    </Formik>
-                </View>
-            </View>
-
-        </View >
+                        <Button title='submit' onPress={props.handleSubmit} />
+                    </View>
+                )}
+            </Formik>
+        </View>
     )
-};
-
-export default Contact;
+}
