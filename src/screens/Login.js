86919450359647688c6359b2d6788868d60753d9
John Nielsen <johnhayesnielsen@gmail.com>
2021-01-04 21:16:44 -0700

    added Parkers wonderful login code

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,103 +3,140 @@ import React, { useState } from "react";
 import { useStoreContext } from "../utils/Store";
 import { LOG_IN, LOG_OUT } from "../utils/actions";
 import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  SafeAreaView,
+    Button,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    SafeAreaView,
 } from "react-native";
 import { Link } from "@react-navigation/native";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
+import { Formik } from 'formik';
 
-const Login = ({ route, navigation }) => {
-  const [state, dispatch] = useStoreContext();
+// const Login = ({ route, navigation }) => {
 
-  const prevLoginCheck = async () => {
-    const check = await Auth.loggedIn();
-    console.log("check:");
-    console.log(check);
-    if (check) {
-      dispatch({ type: LOG_IN });
-    }
-  };
+//     return (
+//         <SafeAreaView>
+//             <StatusBar style="auto" />
+//             <Text> Sign in</Text>
+//             <View>
+//                 <TextInput
+//                     name="email"
+//                     placeholder="Email"
+//                     onChangeText={(text) => setEmailInput(text)}
+//                     defaultValue={emailInput}
+//                 />
+//                 <TextInput
+//                     name="password"
+//                     secureTextEntry={true}
+//                     placeholder="Password"
+//                     onChangeText={(text) => setPasswordInput(text)}
+//                     defaultValue={passwordInput}
+//                 />
+//                 <Button title="Submit" onPress={() => handleFormSubmit()} />
+//                 <Button title="Logout" onPress={() => logoutUser()} />
+//             </View>
+//             <View>
+//                 {/* This Link will need to hook up to the sign up page */}
+//                 <Text>
+//                     Need to create an account? Head to our{" "}
+//                     <Link style={styles.linkText} to="/Signup">
+//                         signup page
+//           </Link>
+//                 </Text>
+//             </View>
+//         </SafeAreaView>
+//     );
+// };
 
-  prevLoginCheck();
+// const styles = StyleSheet.create({
+//     linkText: {
+//         color: "blue",
+//     },
+// });
+// export default Login;
 
-  const [login, { error }] = useMutation(LOGIN_USER);
 
-  const [emailInput, setEmailInput] = useState("");
-  const [passwordInput, setPasswordInput] = useState("");
-  const userInput = {
-    email: emailInput,
-    password: passwordInput,
-  };
+// this form will need styles from a global styles sheet. Change name of function to Login
+export default function Login() {
+    // this is where we'll need to pass the login data to the back end.
+    const [state, dispatch] = useStoreContext();
 
-  // submit form
-  const handleFormSubmit = async () => {
-    try {
-      const { data } = await login({
-        variables: { ...userInput },
-      });
-      await Auth.login(data.login.token);
-      dispatch({ type: LOG_IN });
-    } catch (e) {
-      console.error(e);
-      // clear form values
-      setEmailInput("");
-      setPasswordInput("");
-    }
-  };
+    const prevLoginCheck = async () => {
+        const check = await Auth.loggedIn();
+        console.log("check:");
+        console.log(check);
+        if (check) {
+            dispatch({ type: LOG_IN });
+        }
+    };
 
-  const logoutUser = async () => {
-    try {
-      await Auth.logout();
-      dispatch({ type: LOG_OUT });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+    prevLoginCheck();
 
-  return (
-    <SafeAreaView>
-      <StatusBar style="auto" />
-      <Text> Sign in</Text>
-      <View>
-        <TextInput
-          name="email"
-          placeholder="Email"
-          onChangeText={(text) => setEmailInput(text)}
-          defaultValue={emailInput}
-        />
-        <TextInput
-          name="password"
-          secureTextEntry={true}
-          placeholder="Password"
-          onChangeText={(text) => setPasswordInput(text)}
-          defaultValue={passwordInput}
-        />
-        <Button title="Submit" onPress={() => handleFormSubmit()} />
-        <Button title="Logout" onPress={() => logoutUser()} />
-      </View>
-      <View>
-        {/* This Link will need to hook up to the sign up page */}
-        <Text>
-          Need to create an account? Head to our{" "}
-          <Link style={styles.linkText} to="/Signup">
-            signup page
-          </Link>
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
-};
+    const [login, { error }] = useMutation(LOGIN_USER);
 
-const styles = StyleSheet.create({
-  linkText: {
-    color: "blue",
-  },
-});
-export default Login;
+    // const [emailInput, setEmailInput] = useState("");
+    // const [passwordInput, setPasswordInput] = useState("");
+    // const userInput = {
+    //     email: emailInput,
+    //     password: passwordInput,
+    // };
+
+    // submit form
+    const handleFormSubmit = async ({ values }) => {
+        try {
+            const { data } = await login({
+                variables: { ...values },
+            });
+            await Auth.login(data.login.token);
+            dispatch({ type: LOG_IN });
+        } catch (e) {
+            console.error(e);
+            // clear form values
+            setEmailInput("");
+            setPasswordInput("");
+        }
+    };
+
+    const logoutUser = async () => {
+        try {
+            await Auth.logout();
+            dispatch({ type: LOG_OUT });
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+
+    return (
+        <SafeAreaView>
+            <Text>Login</Text>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={(values, actions) => {
+                    actions.resetForm();
+                    handleFormSubmit({ values });
+                }}
+            >
+                {(props) => (
+                    <View>
+                        <TextInput
+                            placeholder='Enter your email'
+                            onChangeText={props.handleChange('email')}
+                            value={props.values.email}
+                        />
+                        <TextInput
+                            placeholder='Create a password'
+                            onChangeText={props.handleChange('password')}
+                            value={props.values.password}
+                        />
+                        <Button title='submit' onPress={props.handleSubmit} />
+                    </View>
+                )}
+            </Formik>
+        </SafeAreaView>
+    )
+}
