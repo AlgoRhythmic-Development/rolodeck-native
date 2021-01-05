7cc06a5c931400cf1be264eb51659dae386e19a6
TRemigi <63087524+TRemigi@users.noreply.github.com>
2021-01-04 22:39:41 -0700

    add query_me to Home screen and test rendering card data

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Alert, Button, StyleSheet, Text, SafeAreaView } from "react-native";
-import { Link } from "@react-navigation/native";
+import { Button, Text, SafeAreaView, View, StyleSheet } from "react-native";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
+import { ADD_CARD } from "../utils/mutations";
 import Auth from "../utils/auth";
 import { useStoreContext } from "../utils/Store";
-import { LOG_IN, LOG_OUT } from "../utils/actions";
+import { LOG_OUT } from "../utils/actions";
 
 const Home = ({ route, navigation }) => {
+  const { data, loading } = useQuery(QUERY_ME);
+  const me = data?.me || [];
+  const card = me?.cards[0];
+  console.log(card);
+
+  // const [addCard, { error }] = useMutation(ADD_CARD);
+
+  // const variables = {
+  //   name: "Taylor Remigi",
+  //   jobTitle: "Full Stack Web Developer",
+  //   phone: "385-424-8121",
+  //   email: "tayremigi@gmail.com",
+  // };
+
   const [state, dispatch] = useStoreContext();
 
   const logoutUser = async () => {
@@ -22,22 +37,34 @@ const Home = ({ route, navigation }) => {
   return (
     <SafeAreaView>
       <StatusBar style="auto" />
-      <Text>Welcome to RoloDeck!</Text>
-      <Text>Join a community of business professionals.</Text>
-      <Text>Log in or sign-up to get started!</Text>
-      <Button
-        title="Get Started"
-        onPress={() => navigation.navigate("Login", { name: "Login" })}
-      />
-
-      <Button
-        title="Test GraphQL"
-        onPress={() => navigation.navigate("FetchTest")}
-      />
-
+      <Text>Hello, {me.username}</Text>
+      <View style={styles.cardContainer}>
+        <Text style={styles.cardTitle}>{card.name}</Text>
+        <Text style={styles.cardText}>{card.jobTitle}</Text>
+        <Text style={styles.cardText}>{card.email}</Text>
+        <Text style={styles.cardText}>{card.phone}</Text>
+      </View>
       <Button title="Log Out" onPress={() => logoutUser()} />
+      {/* <Button title="add card" onPress={() => addCard({ variables })} /> */}
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    margin: 20,
+    padding: 15,
+    backgroundColor: "green",
+  },
+  cardTitle: {
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "red",
+  },
+  cardText: {
+    textAlign: "center",
+    color: "purple",
+  },
+});
 
 export default Home;
