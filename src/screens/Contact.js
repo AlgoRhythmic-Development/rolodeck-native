cb1e622accb73a773db5f8b9a793ff4a19f3d562
probison <parker.robison@hotmail.com>
2021-01-06 18:48:07 -0700

    created form for Contact page

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -3,31 +3,24 @@ import React from 'react';
 import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Formik } from 'formik';
 
-// const Contact = () => {
-
-//     return (
-//         <View>
-//             <Text>Contact Us</Text>
-//             <Text>Fill out the form below to contact us. We'd love to hear your questions, feedback, and ideas. After submiting this form, we will contact you as soon as possible.
-//                 </Text>
-//             <View>
-//                 {/* <ContactForm></ContactForm> */}
-//             </View>
-
-//         </View >
-//     )
-// };
-
-// export default Contact;
- // this form will need styles from a global styles sheet. Change name of function to Login
+ // this form will need styles from a global styles sheet.
  export default function Contact() {
 
-    // this is where we'll need to pass the login data to the back end.
+    // this is where we'll need to pass the login data to the email handler. 
     return(
         <View>
-            <Text>Login</Text>
+            <Text>Contact Us</Text>
+            <Text>Fill out the form below to contact us. We'd love to hear your questions, feedback, and ideas. 
+                After submiting this form, we will contact you as soon as possible. 
+            </Text>
             <Formik
-                initialValues={{ email: '', password: '' }}
+                initialValues={{ 
+                    firstName: '', 
+                    lastName: '', 
+                    contactFormEmail: '', 
+                    subject: '',
+                    message: '' 
+                }}
                 onSubmit={(values, actions) => {
                     actions.resetForm();
                     console.log(values);
@@ -37,15 +30,46 @@ import { Formik } from 'formik';
                     <View> 
   
                         <TextInput
-                            placeholder='Enter your email'
-                            onChangeText={props.handleChange('email')}
-                            value={props.values.email} 
+                            name= 'firstName'
+                            id='firstName'
+                            placeholder='First Name'
+                            onChangeText={props.handleChange('firstName')}
+                            value={props.values.firstName} 
                         />  
   
                         <TextInput
-                            placeholder='Create a password'
-                            onChangeText={props.handleChange('password')}
-                            value={props.values.password} 
+                            name= 'lastName'
+                            id='lastName'
+                            placeholder='Last Name'
+                            onChangeText={props.handleChange('lastName')}
+                            value={props.values.lastName} 
+                        />   
+
+                        <TextInput
+                            textContentType='emailAddress'
+                            name= 'contactFormEmail'
+                            id='contactFormEmail'
+                            placeholder='Enter email'
+                            onChangeText={props.handleChange('contactFormEmail')}
+                            value={props.values.contactFormEmail} 
+                        />   
+
+                        <TextInput
+                            name="subject"
+                            id="subject"
+                            placeholder='Enter a subject line'
+                            onChangeText={props.handleChange('subject')}
+                            value={props.values.subject} 
+                        />   
+
+                        <TextInput
+                            name="message"
+                            id="message"
+                            placeholder='Message'
+                            multiline={true}
+                            numberOfLines={4}
+                            onChangeText={props.handleChange('message')}
+                            value={props.values.message} 
                         />   
   
                         <Button title='submit' onPress={props.handleSubmit} />
