3ed276809857a37b2e827d8ba0ef4768b4c8414a
Jacob Corum <cor15031@byui.edu>
2021-01-06 21:17:32 -0700

    fixed card after merge

--- src/screens/Collection.js
+++ src/screens/Collection.js
@@ -5,39 +5,7 @@ import { QUERY_CARDS } from '../utils/queries';
 import { FlatList } from 'react-native-gesture-handler';
 import Card from '../components/Card';
 
-const Item = ({ card }) => (
-  <FlipCard
-    friction={6}
-    perspective={1000}
-    flipHorizontal={true}
-    flipVertical={false}
-    flip={false}
-    clickable={true}
-  >
-    {/* Face Side */}
-    <View style={styles.card}>
-      <Text style={styles.title}>{card.randomInfo.company.name}</Text>
-      <Text style={styles.item}>{card.randomInfo.company.catchPhrase}</Text>
-      <Text style={styles.item}>{card.randomInfo.name}</Text>
-      <Text style={styles.item}>{card.randomInfo.website}</Text>
-      <Text style={styles.item}>{card.randomInfo.phone}</Text>
-      <Text style={styles.item}>{card.randomInfo.email}</Text>
-    </View>
-    {/* Back Side */}
-    <View style={styles.back}>
-      <Text style={styles.title}>Back Side</Text>
-    </View>
-    {/* <View style={styles.card}>
-      <Image
-        source={{
-          uri: card.randomImage
-        }}
-      />
-    </View> */}
-  </FlipCard>
-);
-
-const Card = () => {
+const Collection = () => {
   // let id = '5fd45172ff34161dc421c37e'
   // const { data, loading } = useQuery(QUERY_CARD, { variables: { id } })
   // const cardData = data?.card || []
