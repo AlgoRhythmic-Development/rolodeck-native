9ab82d016455a269ceed577762912a75cc9f524a
John Nielsen <johnhayesnielsen@gmail.com>
2021-01-12 11:29:47 -0700

    updated Search with Formik

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,81 +3,95 @@ import { Alert, Button, StatusBar, Text, TextInput, View } from "react-native";
 import { useLazyQuery } from "@apollo/client";
 import { QUERY_USER_CARDS } from "../../utils/queries.js";
 import ResultsModal from "../SearchResultsModal";
+import { Formik } from "formik";
+
 
 const Search = ({ addCollectedCard, collectedCards }) => {
-  const [searchCards, { data }] = useLazyQuery(QUERY_USER_CARDS);
-  const initialResults = data?.userCards || [];
+    const [searchCards, { data }] = useLazyQuery(QUERY_USER_CARDS);
+    const initialResults = data?.userCards || [];
 
-  let filteredResults = [];
+    let filteredResults = [];
 
-  const [finalResults, setFinalResults] = useState(filteredResults);
+    const [finalResults, setFinalResults] = useState(filteredResults);
 
-  const filterResults = () => {
-    filteredResults = [];
+    const filterResults = () => {
+        filteredResults = [];
 
-    for (let i = 0; i < initialResults.length; i++) {
-      let isNewCard = true;
+        for (let i = 0; i < initialResults.length; i++) {
+            let isNewCard = true;
 
-      for (let j = 0; j < collectedCards.length; j++) {
-        if (initialResults[i]._id === collectedCards[j]._id) {
-          isNewCard = false;
-          break;
-        } else {
-          continue;
+            for (let j = 0; j < collectedCards.length; j++) {
+                if (initialResults[i]._id === collectedCards[j]._id) {
+                    isNewCard = false;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+
+            if (isNewCard === false) {
+                continue;
+            } else {
+                filteredResults.push(initialResults[i]);
+            }
         }
-      }
 
-      if (isNewCard === false) {
-        continue;
-      } else {
-        filteredResults.push(initialResults[i]);
-      }
-    }
+        setFinalResults(filteredResults);
+    };
 
-    setFinalResults(filteredResults);
-  };
+    useEffect(() => {
+        filterResults();
+    }, [data]);
 
-  useEffect(() => {
-    filterResults();
-  }, [data]);
+    // use State to handle ResultsModal
+    // set initial show state to false
+    const [show, setShow] = useState(false);
 
-  // use State to handle ResultsModal
-  // set initial show state to false
-  const [show, setShow] = useState(false);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+        await searchCards({
+            variables: { name: e.target.nameInput.value },
+        });
 
-    await searchCards({
-      variables: { name: e.target.nameInput.value },
-    });
+        setShow(true);
+    };
 
-    setShow(true);
-  };
+    return (
+        <View>
+            <ResultsModal
+                show={show}
+                setShow={setShow}
+                results={finalResults}
+                addCollectedCard={addCollectedCard}
+            />
 
-  return (
-    <View>
-      <ResultsModal
-        show={show}
-        setShow={setShow}
-        results={finalResults}
-        addCollectedCard={addCollectedCard}
-      />
-
-      <Form inline onSubmit={handleSubmit}>
-        <Form.Label htmlFor="nameInput" srOnly>
-          Name
-        </Form.Label>
-        <Form.Control
-          id="nameInput"
-          placeholder="Enter a name to search for cards"
-        />
-        <Button type="submit">
-          Search
-        </Button>
-      </Form>
-    </View>
-  );
+            <Formik
+                initialValues={{
+                    searchInput: '',
+                }}
+                onSubmit={(values, actions) => {
+                    handleFormSubmit({ values });
+                    // actions.resetForm();
+                }}
+            >
+                {(props) => (
+                    <View>
+                        <TextInput
+                        name='seearchInput'
+                        id='searchInput'
+                        placeholder='Who are you looking for?'
+                        onChangeText={props.handleChange('searchInput')}
+                        value={props.values.searchInput}
+                        />
+                    </View>
+                )}
+                <Button type="submit">
+                    Search
+                </Button>
+            </Formik>
+        </View>
+    );
 };
 
 export default Search;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Button, Text, SafeAreaView, View } from "react-native";
-import { useQuery, useLazyQuery } from "@apollo/client";
+import { useQuery, useLazyQuery, useMutation } from "@apollo/client";
 import { QUERY_ME, QUERY_USERS } from "../utils/queries";
 import Auth from "../utils/auth";
 import { useStoreContext } from "../utils/Store";
@@ -11,101 +11,130 @@ import AnimatedLoader from "react-native-animated-loader";
 import Card from "../components/Card";
 import StatusModal from "../components/StatusModal";
 import Search from "../components/Search";
+import { ADD_COLLECTED_CARD } from "../utils/mutations";
+
 
 
 const Home = ({ route, navigation }) => {
-  // Since we're working with multiple queries in one functional component,
-  // we need to give our destructured data properties different names
-  // or it will throw an error saying data is already defined
-  const { data: meData, loading: meLoading } = useQuery(QUERY_ME);
-  const me = meData?.me || {};
-  const card = meData?.me.cards[0] || {};
+    // Since we're working with multiple queries in one functional component,
+    // we need to give our destructured data properties different names
+    // or it will throw an error saying data is already defined
+    const { data: meData, loading: meLoading } = useQuery(QUERY_ME);
+    const me = meData?.me || {};
+    const card = meData?.me.cards[0] || {};
 
-  // ***************START OF MODAL USAGE EXAMPLE*********************
+    // ***************START OF MODAL USAGE EXAMPLE*********************
 
-  // show/setShow determine modal visibility
-  const [show, setShow] = useState(false);
+    // show/setShow determine modal visibility
+    const [show, setShow] = useState(false);
 
-  // modalStatus is a message that is passed through props
-  // to be displayed in modal
-  const [modalStatus, setModalStatus] = useState("");
+    // modalStatus is a message that is passed through props
+    // to be displayed in modal
+    const [modalStatus, setModalStatus] = useState("");
 
-  // modalData is any other data you want passed to the modal and displayed
-  // under the status message
-  const [modalData, setModalData] = useState("");
+    // modalData is any other data you want passed to the modal and displayed
+    // under the status message
+    const [modalData, setModalData] = useState("");
 
-  // set up a lazyQuery which will run a callback function
-  // setting our modal state
-  // *** this query is not necessary for this screen so this is
-  // for demo purposes only***
-  const [queryUsers, { error, loading: usersLoading }] = useLazyQuery(
-    QUERY_USERS,
-    {
-      onCompleted: (data) => {
-        if (!error) {
-          setModalStatus("Success!");
-          const user = data.users[0].username;
-          setModalData(user);
-          setShow(true);
-        } else {
-          setModalStatus("Error");
-          setModalData("Couldn't fetch user data...");
-          setShow(true);
+    // set up a lazyQuery which will run a callback function
+    // setting our modal state
+    // *** this query is not necessary for this screen so this is
+    // for demo purposes only***
+    const [queryUsers, { error, loading: usersLoading }] = useLazyQuery(
+        QUERY_USERS,
+        {
+            onCompleted: (data) => {
+                if (!error) {
+                    setModalStatus("Success!");
+                    const user = data.users[0].username;
+                    setModalData(user);
+                    setShow(true);
+                } else {
+                    setModalStatus("Error");
+                    setModalData("Couldn't fetch user data...");
+                    setShow(true);
+                }
+            },
         }
-      },
-    }
-  );
+    );
 
-  // see return jsx for <StatusModal> and props
+    // see return jsx for <StatusModal> and props
 
-  // *********************END OF MODAL USAGE EXAMPLE*************************
+    // *********************END OF MODAL USAGE EXAMPLE*************************
 
-  const [state, dispatch] = useStoreContext();
+    const [state, dispatch] = useStoreContext();
 
-  const logoutUser = async () => {
-    try {
-      await Auth.logout();
-      dispatch({ type: LOG_OUT });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+    const logoutUser = async () => {
+        try {
+            await Auth.logout();
+            dispatch({ type: LOG_OUT });
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar style="auto" />
-      {meLoading ? (
-        <AnimatedLoader
-          visible={true}
-          source={require("../assets/lottie/contact-fill.json")}
-          animationStyle={{ width: "100%", height: "100%" }}
-          speed={1}
-        />
-      ) : (
-        <>
-          <Text>Hello, {me.username}</Text>
-          <Card isHome={true} cardInfo={card} />
-          <View>
-            <Button
-              style={{ marginTop: "15%" }}
-              title="Log Out"
-              onPress={() => logoutUser()}
-            />
-            <StatusModal
-              show={show}
-              setShow={setShow}
-              status={modalStatus}
-              data={modalData}
-            />
-            <Button
-              title="Query Users and Test Modal"
-              onPress={() => queryUsers()}
-            />
-          </View>
-        </>
-      )}
-    </SafeAreaView>
-  );
+    const { collectedCards } = state;
+
+
+    const [addCollectedCard] = useMutation(ADD_COLLECTED_CARD, {
+        update(cache, { data: { addCollectedCard } }) {
+          try {
+            //could potentially not exist yet, so wrap in a try...catch
+            const { me } = cache.readQuery({ query: QUERY_MY_COLLECTION });
+            cache.writeQuery({
+              query: QUERY_MY_COLLECTION,
+              data: {
+                me: {
+                  ...me,
+                  collectedCards: [addCollectedCard, ...collectedCards],
+                },
+              },
+            });
+          } catch (e) {
+            console.error(e);
+          }
+        },
+      });
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar style="auto" />
+            {meLoading ? (
+                <AnimatedLoader
+                    visible={true}
+                    source={require("../assets/lottie/contact-fill.json")}
+                    animationStyle={{ width: "100%", height: "100%" }}
+                    speed={1}
+                />
+            ) : (
+                    <>
+                        <Text>Hello, {me.username}</Text>
+                        <Card isHome={true} cardInfo={card} />
+                        <View>
+                            <Button
+                                style={{ marginTop: "15%" }}
+                                title="Log Out"
+                                onPress={() => logoutUser()}
+                            />
+                            <StatusModal
+                                show={show}
+                                setShow={setShow}
+                                status={modalStatus}
+                                data={modalData}
+                            />
+                            <Button
+                                title="Query Users and Test Modal"
+                                onPress={() => queryUsers()}
+                            />
+                            <Search
+                                addCollectedCard={addCollectedCard}
+                                collectedCards={collectedCards}
+                            />
+                        </View>
+                    </>
+                )}
+        </SafeAreaView>
+    );
 };
 
 export default Home;
