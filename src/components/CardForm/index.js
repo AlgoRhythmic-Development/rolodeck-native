9e276faf49348d4b7726c090b486de5d33bd6517
TRemigi <63087524+TRemigi@users.noreply.github.com>
2021-01-16 16:27:29 -0700

    create form, begin testing

--- src/components/CardForm/index.js
+++ src/components/CardForm/index.js
@@ -1,236 +1,205 @@
-import React from "react";
-import { Alert, Button, Keyboard, StatusBar, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
-import { Formik } from 'formik';
-import {useLazyQuery, useMutation} from '@apollo/client';
-import { ADD_COLLECTED_CARD } from "../../utils/mutations";
-
-// const CardForm = () => {
-    // This is stuff that we will need to add in/fix later.
-    // const [formState, setFormState] = useState({
-    //     // logoUrl: "",
-    //     companyName: "",
-    //     tagline: "",
-    //     name: "",
-    //     jobTitle: "",
-    //     website: "",
-    //     phone: "",
-    //     email: "",
-    //   });
-    
-    //   const [errorMessage, setErrorMessage] = useState("");
-    
-    //   const [addCard, { error }] = useMutation(ADD_CARD, {
-    //     update(cache, { data: { addCard } }) {
-    //       try {
-    //         //could potentially not exist yet, so warap in a try...catch
-    //         const { cards } = cache.readQuery({ query: QUERY_CARDS });
-    //         cache.writeQuery({
-    //           query: QUERY_CARDS,
-    //           data: { cards: [addCard, ...cards] },
-    //         });
-    //       } catch (e) {
-    //         console.error(e);
-    //       }
-    
-    //       //update me object's cache, appending new card to the end of the array
-    //       const { me } = cache.readQuery({ query: QUERY_ME });
-    //       cache.writeQuery({
-    //         query: QUERY_ME,
-    //         data: { me: { ...me, cards: [...me.cards, addCard] } },
-    //       });
-    //     },
-    //   });
-    
-    //   const handleChange = (event) => {
-    //     const { name, value } = event.target;
-    
-    //     setFormState({
-    //       ...formState,
-    //       [name]: value,
-    //     });
-    //   };
-    
-    //   // use State to handle SuccessModal
-    //   // set initial show state to false
-    //   const [show, setShow] = useState(false);
-    
-    //   const isValidPhoneInput = (text) => {
-    //     const isValid = validatePhone(text);
-    //     return isValid;
-    //   };
-    
-    
-    //   const isValidEmailInput = (text) => {
-    //     const isValid = validateEmail(text);
-    //     return isValid;
-    //   };
-    
-    //   //handler for the card form
-    //   const handleFormSubmit = async (event) => {
-    //     event.preventDefault();
-    
-    //     const isValidPhone = isValidPhoneInput(formState.phone);
-    //     const isValidEmail = isValidEmailInput(formState.email);
-    //     const hasName = formState.name;
-    //     const hasTitle = formState.jobTitle;
-    
-    //     try {
-    //       //validate required fields
-    //       if (!isValidEmail) {
-    //         setErrorMessage("Please enter a valid email address.");
-    //         return;
-    //       } else if (!isValidPhone) {
-    //         setErrorMessage("Please enter a valid phone number.");
-    //         return;
-    //       } else if (hasName.length < 3) {
-    //         setErrorMessage("Please enter a full name.");
-    //         return;
-    //       } else if (hasTitle.length < 3) {
-    //         setErrorMessage("Please enter a job title.");
-    //         return;
-    //       } else {
-    //         setErrorMessage("");
-    //       }
-    
-    //       //add card to database
-    //       await addCard({
-    //         variables: { ...formState },
-    //       });
-    
-    //       // clear form value
-    //       setFormState({
-    //         // logoUrl: "",
-    //         companyName: "",
-    //         tagline: "",
-    //         name: "",
-    //         jobTitle: "",
-    //         website: "",
-    //         phone: "",
-    //         email: "",
-    //       });
-    
-    //       // display SuccessModal
-    //       setShow(true);
-    //     } catch (e) {
-    //       console.error(e);
-    //     }
-    //   };
-
-
-//     return (
-//         <View>    
-//             <StatusBar style="auto" />
-//             <Text> Fill out the form below</Text>
-//             <View>
-//                 <TextInput placeholder="Company Name" />
-//                 <TextInput placeholder="Company's Tagline" />
-//                 <TextInput placeholder="Full Name" />
-//                 <TextInput placeholder="Job Title" />
-//                 <TextInput placeholder="Company Website" />
-//                 <TextInput placeholder="Phone Number" />
-//                 <TextInput placeholder="Email Address" />
-//                 <Button 
-//                     title="Submit" 
-//                     onPress={() => Alert.alert("Under construction")} 
-//                 />
-//             </View>
-//         </View>
-//     )
-// }
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  Keyboard,
+  StatusBar,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  View,
+  StyleSheet,
+} from "react-native";
+import { Formik } from "formik";
+import { useMutation } from "@apollo/client";
+import { ADD_CARD } from "../../utils/mutations";
+import { QUERY_CARDS, QUERY_ME } from "../../utils/queries";
+// component imports
+import StatusModal from "../StatusModal";
 
 export default function Cardform() {
+  const [show, setShow] = useState(false);
+  const [modalStatus, setModalStatus] = useState("");
+  const [modalData, setModalData] = useState("");
 
-    // this is where we'll need to pass the login data to the back end.
+  const [addCard, { error, loading }] = useMutation(ADD_CARD, {
+    onCompleted: (data) => {
+      if (!error) {
+        setModalStatus("Card Created!");
+        setModalData("See your new card on the Home screen");
+        setShow(true);
+      } else {
+        setModalStatus("Error");
+        setModalData("Couldn't create card...");
+        setShow(true);
+      }
+    },
+    update(cache, { data: { addCard } }) {
+      try {
+        //could potentially not exist yet, so wrap in a try...catch
+        const { cards } = cache.readQuery({ query: QUERY_CARDS });
+        cache.writeQuery({
+          query: QUERY_CARDS,
+          data: { cards: [addCard, ...cards] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+      try {
+        //update me object's cache, appending new card to the end of the array
+        const { me } = cache.readQuery({ query: QUERY_ME });
+        cache.writeQuery({
+          query: QUERY_ME,
+          data: { me: { ...me, cards: [...me.cards] } },
+        });
+      } catch (e) {
+        console.error(error);
+      }
+    },
+  });
 
-    // edit this form to include formik info.
+  // submit form
+  const handleFormSubmit = async ({ values }) => {
+    try {
+      await addCard({
+        variables: { input: values },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-    // query stuff
+  return (
+    <View>
+      <StatusModal
+        show={show}
+        setShow={setShow}
+        status={modalStatus}
+        data={modalData}
+      />
+      <Text>Create a New Card</Text>
+      <Formik
+        initialValues={{
+          companyName: "",
+          tagline: "",
+          name: "",
+          jobTitle: "",
+          website: "",
+          phone: "",
+          email: "",
+        }}
+        onSubmit={(values, actions) => {
+          console.log(values);
+          handleFormSubmit({ values });
+          actions.resetForm();
+        }}
+      >
+        {(props) => (
+          // this one does close the keyboard but after every keystroke.
+          // <TouchableWithoutFeedback onPress={Keyboard.dismiss()}>
+          <View style={styles.formContainer}>
+            <TextInput
+              style={styles.formInput}
+              name="companyName"
+              id="companyName"
+              placeholder="Enter company name"
+              onChangeText={props.handleChange("companyName")}
+              value={props.values.companyName}
+            />
 
-    return(
-        <View>
-            <Text>Fill out the form below</Text>
-            <Formik
-                initialValues={{ 
-                    companyName: "",
-                    tagline: "",
-                    name: "",
-                    jobTitle: "",
-                    website: "",
-                    phone: "",
-                    email: "",
-                }}
+            <TextInput
+              style={styles.formInput}
+              name="tagline"
+              id="tagline"
+              placeholder="Enter tagline"
+              onChangeText={props.handleChange("tagline")}
+              value={props.values.tagline}
+            />
 
-                onSubmit={(values, actions) => {
-                    actions.resetForm();
-                    console.log(values);
-                }}
+            <TextInput
+              style={styles.formInput}
+              name="name"
+              id="name"
+              placeholder="Enter Full Name"
+              onChangeText={props.handleChange("name")}
+              value={props.values.name}
+            />
+
+            <TextInput
+              style={styles.formInput}
+              name="jobTitle"
+              id="jobTitle"
+              placeholder="Enter Job Title"
+              onChangeText={props.handleChange("jobTitle")}
+              value={props.values.jobTitle}
+            />
+
+            <TextInput
+              style={styles.formInput}
+              name="website"
+              id="website"
+              placeholder="Enter Website"
+              onChangeText={props.handleChange("website")}
+              value={props.values.website}
+            />
+
+            <TextInput
+              style={styles.formInput}
+              name="phone"
+              id="phone"
+              placeholder="Enter phone number"
+              keyboardType="numeric"
+              onChangeText={props.handleChange("phone")}
+              value={props.values.phone}
+            />
+            <TextInput
+              style={styles.formInput}
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+              onChangeText={props.handleChange("email")}
+              value={props.values.email}
+            />
+            <TouchableHighlight
+              style={{ ...styles.submitButton }}
+              onPress={props.handleSubmit}
             >
-                {(props) => (
-                    // this one does close the keyboard but after every keystroke.
-                    // <TouchableWithoutFeedback onPress={Keyboard.dismiss()}>
-                    <View> 
-                        
-                        <TextInput
-                            name="companyName"
-                            id="companyName"
-                            placeholder='Enter company name'
-                            onChangeText={props.handleChange('companyName')}
-                            value={props.values.companyName} 
-                        /> 
+              <Text style={{ ...styles.submitButton }}>Submit Changes</Text>
+            </TouchableHighlight>
+          </View>
+          // </TouchableWithoutFeedback>
+        )}
+      </Formik>
+    </View>
+  );
+}
 
-                        <TextInput
-                            name="tagline"
-                            id="tagline"
-                            placeholder='Enter tagline'
-                            onChangeText={props.handleChange('tagline')}
-                            value={props.values.tagline} 
-                        /> 
-
-                        <TextInput
-                            name='name'
-                            id='name'
-                            placeholder='Enter Full Name'
-                            onChangeText={props.handleChange('name')}
-                            value={props.values.name} 
-                        /> 
-
-                        <TextInput
-                            name='jobTitle'
-                            id='jobTitle'
-                            placeholder='Enter Job Title'
-                            onChangeText={props.handleChange('jobTitle')}
-                            value={props.values.jobTitle} 
-                        /> 
-
-                        <TextInput
-                            name='website'
-                            id='website'
-                            placeholder='Enter Website'
-                            onChangeText={props.handleChange('website')}
-                            value={props.values.website} 
-                        /> 
-
-                        <TextInput
-                            name='phone'
-                            id='phone'
-                            placeholder='Enter phone number'
-                            keyboardType='numeric'
-                            onChangeText={props.handleChange('phone')}
-                            value={props.values.phone} 
-                        /> 
-                        <TextInput
-                            name='email'
-                            id='email'
-                            placeholder='Enter your email address'
-                            onChangeText={props.handleChange('email')}
-                            value={props.values.email} 
-                        />    
-  
-                        <Button title='submit' onPress={props.handleSubmit} />
-                    </View>
-                    // </TouchableWithoutFeedback>
-                )}
-            </Formik>
-        </View>
-    )
-  }
+const styles = StyleSheet.create({
+  formContainer: {
+    borderRadius: 20,
+    backgroundColor: "white",
+    marginBottom: 10,
+    marginHorizontal: 5,
+    paddingTop: 6,
+  },
+  formInput: {
+    borderRadius: 20,
+    backgroundColor: "#F6F6F6",
+    borderColor: "#8D8D8D",
+    margin: 3,
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    fontSize: 25,
+  },
+  submitButton: {
+    color: "white",
+    fontSize: 20,
+    alignItems: "center",
+    backgroundColor: "#2196F3",
+    borderRadius: 20,
+    margin: 3,
+    padding: 5,
+    elevation: 2,
+  },
+});
--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,26 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Alert, Button, Keyboard, SafeAreaView, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Alert,
+  Button,
+  Keyboard,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import CardForm from "../components/CardForm";
 
-const Create = ({route, navigation}) => {
-
-    // We'll add this in at a later time.
-    // redirect to my cards page if username is the logged-in user's
-    // if (Auth.loggedIn()) {
-    //     return <Redirect to="/cards" />;
-    // }
-
-    return (
-        <SafeAreaView>
-            <Text>Create a Business Card</Text> 
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View>
-                    <CardForm />
-                </View>
-            </TouchableWithoutFeedback>   
-        </SafeAreaView>
-    )   
+const Create = ({ route, navigation }) => {
+  return (
+    <SafeAreaView>
+      <Text>Create a Business Card</Text>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View>
+          <CardForm />
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  );
 };
 
-export default Create
+export default Create;
