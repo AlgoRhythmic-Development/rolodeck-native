32ac81f1881864bd36ab50a645d1a748af67ac45
John Nielsen <johnhayesnielsen@gmail.com>
2021-01-18 15:27:11 -0700

    added Search screen and fake search stuff

--- src/screens/Collection.js
+++ src/screens/Collection.js
@@ -1,31 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, Text, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { useQuery } from '@apollo/client';
 import { QUERY_MY_COLLECTION } from '../utils/queries';
 import { FlatList } from 'react-native-gesture-handler';
 import Card from '../components/Card';
 import { useStoreContext } from '../utils/Store';
 import SET_ID from '../utils/reducers';
+import { ApplicationProvider, Avatar } from '@ui-kitten/components';
+import { mapping, light as lightTheme } from '@eva-design/eva';
 
-const Collection = () => {
-  // const [state] = useStoreContext();
-  // const { scannedId } = state;
 
-  const { data } = useQuery(QUERY_MY_COLLECTION);
-  const collectedCards = data?.collectedCards || [];
-  console.log(collectedCards)
+  const Collection = () => {
+    // const [state] = useStoreContext();
+    // const { scannedId } = state;
 
-  const renderItem = ({ item }) => <Card cardInfo={item} />;
+    const { data } = useQuery(QUERY_MY_COLLECTION);
+    const collectedCards = data?.collectedCards || [];
+    console.log(collectedCards)
 
-  return (
-    <View>
-      <FlatList
-        data={collectedCards}
-        renderItem={renderItem}
-        keyExtractor={(item) => item._id}
-      />
-    </View>
-  );
-};
+    const renderItem = ({ item }) => <Card cardInfo={item} />;
+
+    return (
+      <SafeAreaView>
+        <FlatList
+          data={collectedCards}
+          renderItem={renderItem}
+          keyExtractor={(item) => item._id}
+        />
+      </SafeAreaView>
+    );
+  };
 
 export default Collection;
